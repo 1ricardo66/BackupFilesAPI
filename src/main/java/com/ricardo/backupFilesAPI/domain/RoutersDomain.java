dccdbbf9bc0f651f586a40bd10f2ffd32cb000ba
Ricardo <ricardofps66@gmail.com>
2022-05-14 16:28:31 -0300

    Creating domain file

--- src/main/java/com/ricardo/backupFilesAPI/domain/RoutersDomain.java
+++ src/main/java/com/ricardo/backupFilesAPI/domain/RoutersDomain.java
@@ -1,12 +1,75 @@
 package com.ricardo.backupFilesAPI.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Table;
+
+import org.springframework.data.annotation.Id;
 
 @Entity
 @Table(name="TB_Router")
 public class RoutersDomain {
 
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	
+	private String host;
+
+	private int port;
+	
+	private String username;
+	
+	private String password;
+	
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
